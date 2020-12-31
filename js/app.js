48577c4ed4c1b5400c4a14ece697111dceb3ee3e
aomine <aomine1745@gmail.com>
2020-12-31 16:43:53 -0500

    v1.0

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const box = document.querySelector(".box");
+let shadow = "";
+
+for (let i = 0; i < 500; i++) {
+  shadow += (shadow ? "," : "") + i * 1 + "px " + i * 1 + "px 0 #e8305b";
+}
+
+box.style.boxShadow = shadow;
